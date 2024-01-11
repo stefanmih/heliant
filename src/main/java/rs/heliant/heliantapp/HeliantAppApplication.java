e828b0d441d61b32301bc44c4397b3cab88177e0
stefanmih <bjtnpn4tgrepolis@gmail.com>
2024-01-11 19:59:58 +0100

    initialization

---
+++ src/main/java/rs/heliant/heliantapp/HeliantAppApplication.java
@@ -0,0 +1,14 @@
+package rs.heliant.heliantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliantAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeliantAppApplication.class, args);
+    }
+
+}
